049d9441ebfd5464c061aed565060b38729fe6a8
iamucil <ecko.ucil@gmail.com>
2021-08-13 01:29:59 +0700

    handle panic error when obtaining utsname from os

--- app/uname.go
+++ app/uname.go
@@ -3,10 +3,19 @@ package app
 import (
 	"bytes"
 	"fmt"
+	"net"
+	"runtime"
+	"strings"
 	"syscall"
 )
 
 func unameString() (string, error) {
+	os := runtime.GOOS
+	switch os {
+	case "windows", "darwin":
+		return getNetworkHardwareName(), nil
+	}
+
 	utsname := syscall.Utsname{}
 	err := syscall.Uname(&utsname)
 	if err != nil {
@@ -31,4 +40,56 @@ func utsStringToString(utsStr [65]int8) string {
 	}
 	s = s[:bytes.IndexByte(s, 0)]
 	return string(s)
+}
+
+func getNetworkHardwareName() string {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return ""
+	}
+
+	var currentIP, currentNetworkHardwareName string
+
+	for _, address := range addrs {
+
+		// check the address type and if it is not a loopback the display it
+		// = GET LOCAL IP ADDRESS
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				currentIP = ipnet.IP.String()
+			}
+		}
+	}
+	// get all the system's or local machine's network interfaces
+	interfaces, _ := net.Interfaces()
+	for _, interf := range interfaces {
+
+		if addrs, err := interf.Addrs(); err == nil {
+			for _, addr := range addrs {
+				// only interested in the name with current IP address
+				if strings.Contains(addr.String(), currentIP) {
+					currentNetworkHardwareName = interf.Name
+				}
+			}
+		}
+	}
+
+	// extract the hardware information base on the interface name
+	// capture above
+	netInterface, err := net.InterfaceByName(currentNetworkHardwareName)
+
+	if err != nil {
+		return ""
+	}
+
+	macAddress := netInterface.HardwareAddr
+
+	// verify if the MAC address can be parsed properly
+	hwAddr, err := net.ParseMAC(macAddress.String())
+
+	if err != nil {
+		return ""
+	}
+	return hwAddr.String()
 }
